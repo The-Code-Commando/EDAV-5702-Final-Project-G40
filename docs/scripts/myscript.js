548c24f4a5184e27fc8e59979f454e159506cbd7
vg2595 <vg2595@columbia.edu>
2023-12-14 15:42:57 -0500

    Updated Interactive

--- docs/scripts/myscript.js
+++ docs/scripts/myscript.js
@@ -1,11 +1,7 @@
 function loadData(url) {
   return d3.csv(url).then(function(data) {
-    // Map each row to an object
     const objects = data.map(function(row) {
-
       return {
-        // player: row.Player,
-        // out: row.OUT,
         caught: row.Caught,
         bowled: row.Bowled,
         stumped: row.Stumped,
@@ -20,7 +16,6 @@ function loadData(url) {
   });
 }
 
-
 const url = "https://raw.githubusercontent.com/Abhik-Biswas/Project-CSV/main/Data_Player_dismissial_preprocessed.csv";
 loadData(url).then(function(objects) {
   console.log(objects);
@@ -30,97 +25,120 @@ loadData(url).then(function(objects) {
     height = 450,
     margin = 75;
 
-
   const radius = Math.min(width, height) / 2 - margin;
 
-
   const svg = d3.select("div#plot")
-  .append("svg")
+    .append("svg")
     .attr("width", width)
     .attr("height", height)
-  .append("g")
+    .append("g")
     .attr("transform", `translate(295, 220)`);
 
   const color = d3.scaleOrdinal()
-  .domain(["caught", "bowled", "stumped", "caught_behind", "lbw", "run_out", "others"])
-  .range(d3.schemeDark2);
+    .domain(["caught", "bowled", "stumped", "caught_behind", "lbw", "run_out", "others"])
+    .range(d3.schemeDark2);
 
+  const tooltip = d3.select("div#plot")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-function update(data) {
+  function update(data) {
+    const pie = d3.pie()
+      .value(function(d) { return d[1]; })
+      .sort(function(a, b) { return d3.ascending(a.key, b.key); });
+    const data_ready = pie(Object.entries(data));
 
-  // Compute the position of each group on the pie:
-  const pie = d3.pie()
-    .value(function(d) {return d[1]; })
-    .sort(function(a, b) { return d3.ascending(a.key, b.key);} )
-  const data_ready = pie(Object.entries(data))
-
-  // map to data
-  const u = svg.selectAll("path")
-    .data(data_ready)
-
-  u
-    .join('path')
-    .transition()
-    .duration(1000)
-    .attr('d', d3.arc()
+    const arc = d3.arc()
       .innerRadius(0)
-      .outerRadius(radius)
-    )
-    .attr('fill', function(d){ return(color(d.data[0])) })
-    .attr("stroke", "white")
-    .style("stroke-width", "2px")
-    .style("opacity", 1)
+      .outerRadius(radius);
 
- // Add annotations with arrows
+    const u = svg.selectAll("path")
+      .data(data_ready);
+
+    u
+      .join('path')
+      .on("mouseover", function(event, d) {
+        const percentage = d3.format(".1%")(d.data[1] / d3.sum(data_ready, d => d.data[1]));
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip.html(`${d.data[0]}: ${d.data[1]} (${percentage})`)
+          .style("left", (event.pageX + 5) + "px")
+          .style("top", (event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+        tooltip.transition().duration(500).style("opacity", 0);
+      })
+      .transition()
+      .duration(1000)
+      .attr('d', arc)
+      .attr('fill', function(d) { return color(d.data[0]); })
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .style("opacity", 1);
+
     svg.selectAll(".annotation-line")
       .data(data_ready)
       .join("line")
+      .on("mouseover", function(event, d) {
+        const percentage = d3.format(".1%")(d.data[1] / d3.sum(data_ready, d => d.data[1]));
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip.html(`${d.data[0]}: ${d.data[1]} (${percentage})`)
+          .style("left", (event.pageX + 5) + "px")
+          .style("top", (event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+        tooltip.transition().duration(500).style("opacity", 0);
+      })
       .transition()
       .duration(1000)
       .attr("class", "annotation-line")
       .attr("x1", function(d) { return d3.arc().innerRadius(radius).outerRadius(radius * 1.16).centroid(d)[0]; })
       .attr("y1", function(d) { return d3.arc().innerRadius(radius).outerRadius(radius * 1.16).centroid(d)[1]; })
       .attr("x2", function(d) { return d3.arc().innerRadius(radius * 0.75).outerRadius(radius * 1.16).centroid(d)[0]; })
-      .attr("y2", function(d) { return d3.arc().innerRadius(radius * 0.75).outerRadius(radius * 1.16).centroid(d)[1]; })
+      .attr("y2", function(d) { return d3.arc().innerRadius(radius * 0.75).outerRadius(radius * 1.16).centroid(d)[1]; })
       .attr("stroke", "black")
       .style("stroke-width", "1.2px");
 
     svg.selectAll(".annotation-text")
       .data(data_ready)
       .join("text")
+      .on("mouseover", function(event, d) {
+        const percentage = d3.format(".1%")(d.data[1] / d3.sum(data_ready, d => d.data[1]));
+        tooltip.transition().duration(200).style("opacity", 0.9);
+        tooltip.html(`${d.data[0]}: ${d.data[1]} (${percentage})`)
+          .style("left", (event.pageX + 5) + "px")
+          .style("top", (event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+        tooltip.transition().duration(500).style("opacity", 0);
+      })
       .transition()
       .duration(1000)
       .attr("class", "annotation-text")
-      .text(function(d) { return `${d.data[0]}: ${d.data[1]}`; })
+      .text(function(d) { return `${d.data[0]}`; })
       .attr("transform", function(d) {
         const pos = d3.arc().innerRadius(radius * 1.23).outerRadius(radius * 1.28).centroid(d);
-        return `translate(${pos[0]}, ${pos[1]})`;
-      })
+        return `translate(${pos[0]}, ${pos[1]})`;})
       .style("text-anchor", "middle")
       .style("font-size", "12px")
       .style("fill", "black");
+  }
 
-}
+  update(data[0]);
 
-// Initialize the plot with the first dataset
+  var dropdown = d3.select("div#plot")
+    .append("select")
+    .attr("class", "dropdown-btn")
+    .on("change", function() {
+      update(data[this.value]);
+    });
 
-update(data[0]);
-
-var dropdown = d3.select("div#plot")
-  .append("select")
-  .attr("class", "dropdown-btn")
-  .on("change", function() {
-    update(data[this.value]);
-  });
-
-dropdown.selectAll("option")
-  .attr("id", "inDropdown")
-  .data(["SR Tendulkar", "KC Sangakkara", "V Kohli", "RT Ponting", "ST Jayasuriya", "DPMD Jayawardene", "Inzamam-Ul-Haq", "JH Kallis", "SC Ganguly", "R Dravid", "MS Dhoni"])
-  .enter().append("option")
-  .attr("value", (d,i)=>i)
-  .text(function(d) { return d; });
-
-
+  dropdown.selectAll("option")
+    .data(["SR Tendulkar", "KC Sangakkara", "V Kohli", "RT Ponting", "ST Jayasuriya", "DPMD Jayawardene", "Inzamam-Ul-Haq", "JH Kallis", "SC Ganguly", "R Dravid", "MS Dhoni"])
+    .enter().append("option")
+    .attr("value", (d, i) => i)
+    .text(function(d) { return d; });
 });
+
 
 
